fda6d46aaf08e29fa42e3917008f89ced735e157
scgallagher <seangallagher135@gmail.com>
2020-04-05 19:39:11 -0400

    added model for monthly bills

--- backend/hermes/models.py
+++ backend/hermes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class MonthlyBill(models.Model):
+
+    name = models.CharField(max_length=200)
+    due_date = models.DateField()
+    amount = models.DecimalField(max_digits=15, decimal_places=2)
+    type = models.CharField(max_length=200)
+    auto_pay = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
