b12dc4f6f1e072797fb08e8b765fbe9b7b9eb46b
scgallagher <seangallagher135@gmail.com>
2020-04-27 22:28:15 -0400

    added endpoint for updating bill

--- backend/hermes/urls.py
+++ backend/hermes/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bills/monthly', views.get_all_monthly_bills, name='get_all_monthly_bills'),
-    path('bills/monthly/<int:id>', views.get_by_id, name='get_by_id')
+    path('bills/monthly/', views.get_all_monthly_bills, name='get_all_monthly_bills'),
+    path('bills/monthly/<int:id>/', views.get_by_id, name='get_by_id'),
+    path('bills/monthly/update/<int:id>/', views.update_bill, name='update_bill')
 ]
--- backend/hermes/views.py
+++ backend/hermes/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.core import serializers
 from django.forms.models import model_to_dict
 from .models import MonthlyBill
@@ -16,3 +16,20 @@ def get_by_id(request, id):
     bill = MonthlyBill.objects.get(pk=id)
 
     return JsonResponse(model_to_dict(bill))
+
+def update_bill(request, id):
+
+    body = request.body.decode('utf-8')
+    new_bill = json.loads(body)
+    print(new_bill.get('name'))
+
+    current_bill = MonthlyBill.objects.get(pk=id)
+    current_bill.name = new_bill.get('name')
+    current_bill.due_date = new_bill.get('due_date')
+    current_bill.amount = new_bill.get('amount')
+    current_bill.type = new_bill.get('type')
+    current_bill.auto_pay = new_bill.get('auto_pay')
+
+    current_bill.save()
+
+    return HttpResponse(status=200)
