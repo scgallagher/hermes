2614f35e1ce3c5e8deaabb5e0679c315b23bd558
scgallagher <seangallagher135@gmail.com>
2020-04-27 21:44:17 -0400

    modified bills/monthly endpoint to include id, added get_by_id

--- backend/hermes/urls.py
+++ backend/hermes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bills/monthly', views.get_all_monthly_bills, name='get_all_monthly_bills')
+    path('bills/monthly', views.get_all_monthly_bills, name='get_all_monthly_bills'),
+    path('bills/monthly/<int:id>', views.get_by_id, name='get_by_id')
 ]
--- backend/hermes/views.py
+++ backend/hermes/views.py
@@ -1,15 +1,18 @@
 from django.http import JsonResponse
 from django.core import serializers
+from django.forms.models import model_to_dict
 from .models import MonthlyBill
 
 import json
 
 def get_all_monthly_bills(request):
 
-    bills = serializers.serialize('json', MonthlyBill.objects.all())
-    bills = json.loads(bills)
-    bills = [bill.get('fields') for bill in bills]
-
-    print(bills[0])
+    bills = [model_to_dict(bill) for bill in MonthlyBill.objects.all()]
 
     return JsonResponse({'bills': bills})
+
+def get_by_id(request, id):
+
+    bill = MonthlyBill.objects.get(pk=id)
+
+    return JsonResponse(model_to_dict(bill))
